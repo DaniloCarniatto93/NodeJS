b926af5fcfaa10f094757701baf1fca0f80f9af0
MirConsult <danilocarniatto@gmail.com>
2016-10-03 14:31:17 -0300

    Implementação da requisição json

--- casadocodigo/app/routes/produtos.js
+++ casadocodigo/app/routes/produtos.js
@@ -1,11 +1,39 @@
 module.exports = function(app){
-	app.get('/produtos',function(req,res){
+	var listaProdutos = function(req,res){
 		var connection = app.infra.connectionFactory();
-		connection.query('select * from livros',function(err,results){
+		var produtosDAO = new app.infra.ProdutosDAO(connection);
+
+		produtosDAO.lista(function(err,results){
+			res.format({
+				html: function(){
+					res.render('produtos/lista',{lista:results});
+				},
+				json: function(){
+					res.json(results);
+				}
+			});
 			res.render('produtos/lista',{lista:results});
 		});
 
 		connection.end();
-		//consulta
-});
+	};	
+		
+		app.get('/produtos',listaProdutos);
+
+		app.get('/produtos/form',function(req,res){
+			res.render('produtos/form');
+		});
+
+		app.post('/produtos',function(req,res){
+
+			var produto = req.body;
+			console.log(produto);
+			var connection = app.infra.connectionFactory();
+		var produtosDAO = new app.infra.ProdutosDAO(connection);
+		produtosDAO.salva(produto,function(erros,results){
+			res.redirect('/produtos');
+		});
+
+		});
+
 }
